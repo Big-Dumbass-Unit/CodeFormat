86089d0930862d4f98d82daf149446ebcda39acb
Loke Vigstrand <loke@Lokes-MacBook.local>
2026-08-06 16:11:40 +0200

    Initial Commit

---
+++ CodeFormat/Program.cs
@@ -0,0 +1,43 @@
+﻿using NDesk.Options;
+
+namespace BDU.Tools.CodeFormat;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        bool help = false;
+        List<string> ignoredPaths = new List<string>();
+
+        OptionSet options = new OptionSet()
+        {
+            { "i|ignore=", "Specifies a folder name to ignore when checking the formats.", ignoredPaths.Add },
+            { "h|help|?", "Shows this message.", v => help = v != null }
+        };
+
+        if (help)
+        {
+            Console.WriteLine("Usage: CodeFormat [OPTIONS]+ path(s) to check");
+            Console.WriteLine("Checks the formatting of a specified folder (and subdirectories) alongside options.\n");
+            Console.WriteLine("Options:");
+            options.WriteOptionDescriptions(Console.Out);
+            return 0;
+        }
+
+        List<string> extra = options.Parse(args);
+        string path = string.Empty;
+
+        if (extra.Count > 0)
+        {
+            path = string.Join(" ", extra.ToArray());
+        }
+        else
+        {
+            throw new ArgumentException("No path(s) specified.");
+        }
+        
+        
+        
+        return 0;
+    }
+}
