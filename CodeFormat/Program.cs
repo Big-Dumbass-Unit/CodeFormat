020ec9fa9ef918fad092ca60c1d201dc0b68d7b3
LokiVig <loke@jenjen.se>
2026-08-07 13:43:25 +0200

    Finish with name checking on all sortsa different types.

--- CodeFormat/Program.cs
+++ CodeFormat/Program.cs
@@ -12,16 +12,15 @@ public static class Program
 
     private static readonly string[] rankNames =
     {
-        "event / event field", "delegate", "readonly field / property", "public field / property", 
+        "event / event field", "delegate", "readonly field / property", "public field / property",
         "protected field / property", "private field / property", "other field / property",
-        "constructor", "Unity message", "public method", "protected method", "private method", 
-        "other method", "nested type"  
+        "constructor", "Unity message", "public method", "protected method", "private method",
+        "other method", "nested type"
     };
 
     private static int violations = 0;
 
     private static bool onCI = Environment.GetEnvironmentVariable("GITHUB_ACTIONS") == "true";
-    private static string root = string.Empty;
     private static ConsoleColor originalColor = Console.ForegroundColor;
 
     public static int Main(string[] args)
@@ -83,27 +82,70 @@ public static class Program
                             previousName = Name(member);
                         }
 
-                        // CheckMemberNaming(file, type, member);
+                        CheckMemberNaming(file, type, member);
+                    }
+                }
+
+                foreach (BaseTypeDeclarationSyntax type in root.DescendantNodes().OfType<BaseTypeDeclarationSyntax>())
+                {
+                    string typeName = type.Identifier.Text;
+
+                    if (type is InterfaceDeclarationSyntax)
+                    {
+                        if (typeName.Length < 2 || typeName[0] != 'I' || !char.IsUpper(typeName[1]))
+                        {
+                            Report(file, type, $"interface '{typeName}' should be named 'I' + PascalCase");
+                        }
+                    }
+                    else if (!IsPascalCase(typeName))
+                    {
+                        Report(file, type, $"type '{typeName}' should be PascalCase");
+                    }
+
+                    if (type is EnumDeclarationSyntax enumType)
+                    {
+                        foreach (EnumMemberDeclarationSyntax enumMember in enumType.Members)
+                        {
+                            if (!IsPascalCase(enumMember.Identifier.Text))
+                            {
+                                Report(file, type, $"{typeName}: enum member '{enumMember.Identifier.Text}' should be PascalCase");
+                            }
+                        }
+                    }
+                }
+
+                foreach (ParameterSyntax parameter in root.DescendantNodes().OfType<ParameterSyntax>())
+                {
+                    string paramName = parameter.Identifier.Text;
+
+                    if (paramName.Length == 0 || paramName == "_")
+                    {
+                        continue;
+                    }
+
+                    if (!IsCamelCase(paramName))
+                    {
+                        Report(file, parameter, $"parameter '{paramName}' should be camelCase");
                     }
                 }
             }
         }
-        
+
         Console.ForegroundColor = violations == 0 ? ConsoleColor.Green : ConsoleColor.Red;
-        Console.WriteLine(violations == 0 
+        Console.WriteLine(violations == 0
             ? "CodeFormat successfully found no violations."
             : $"CodeFormat found {violations} violation(s).");
         Console.ForegroundColor = originalColor;
 
         return violations == 0 ? 0 : 1;
     }
-    
+
     private static void Report(string file, SyntaxNode node, string message)
     {
         int line = node.GetLocation().GetLineSpan().StartLinePosition.Line + 1;
 
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(onCI 
+        Console.WriteLine(onCI
         ? $"::error file={file.Replace('\\', '/')},line={line}::{message}"
         : $"{file}({line}): {message}");
         Console.ForegroundColor = originalColor;
@@ -152,22 +194,48 @@ public static class Program
         };
     }
 
-    // private static void CheckMemberNaming(string file, TypeDeclarationSyntax type, MemberDeclarationSyntax member)
-    // {
-    //     switch (member)
-    //     {
-    //         case FieldDeclarationSyntax f:
-    //             bool isConst = f.Modifiers.Any(SyntaxKind.ConstKeyword);
+    private static void CheckMemberNaming(string file, TypeDeclarationSyntax type, MemberDeclarationSyntax member)
+    {
+        switch (member)
+        {
+            case FieldDeclarationSyntax f:
+                bool isConst = f.Modifiers.Any(SyntaxKind.ConstKeyword);
 
-    //             foreach (VariableDeclaratorSyntax variable in f.Declaration.Variables)
-    //             {
-    //                 string name = variable.Identifier.Text;
+                foreach (VariableDeclaratorSyntax variable in f.Declaration.Variables)
+                {
+                    string name = variable.Identifier.Text;
 
-    //                 if (isConst && !IsPascalCase(name))
-    //                 {
-                        
-    //                 }
-    //             }
-    //     }
-    // }
+                    if (isConst && !IsPascalCase(name))
+                    {
+                        Report(file, variable, $"{type.Identifier}: constant '{name}' should be PascalCase");
+                    }
+                    else if (!isConst && !IsCamelCase(name))
+                    {
+                        Report(file, variable, $"{type.Identifier}: field '{name}' should be camelCase");
+                    }
+                }
+                break;
+
+            case EventFieldDeclarationSyntax e:
+                foreach (VariableDeclaratorSyntax variable in e.Declaration.Variables)
+                {
+                    if (!IsCamelCase(variable.Identifier.Text))
+                    {
+                        Report(file, variable, $"{type.Identifier}: event '{variable.Identifier.Text}' should be camelCase");
+                    }
+                }
+                break;
+
+            case PropertyDeclarationSyntax p when !IsCamelCase(p.Identifier.Text):
+                Report(file, p, $"{type.Identifier}: property '{p.Identifier.Text}' should be camelCase");
+                break;
+
+            case MethodDeclarationSyntax m when !IsUnityMessage(m.Identifier.Text) && !IsPascalCase(m.Identifier.Text):
+                Report(file, m, $"{type.Identifier}: method '{m.Identifier.Text}' should be PascalCase");
+                break;
+        }
+    }
+
+    private static bool IsPascalCase(string name) => char.IsUpper(name[0]) && !name.Contains('_');
+    private static bool IsCamelCase(string name) => char.IsLower(name[0]) && !name.Contains('_');
 }
